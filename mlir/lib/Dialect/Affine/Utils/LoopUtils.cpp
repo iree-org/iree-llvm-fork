cd33d6274116a96ecfea06b52c27edaba34125f6
Matthias Springer <me@m-sp.org>
2023-03-17 02:09:30 +0000

    [mlir][Analysis][NFC] Make BoundType a top-level enum
    `BoundType` is no longer a nested member of `IntegerRelation` but a top-level enum in the `presburger` namespace.
    
    This allows `BoundType` to be predeclared in header files. Nested members cannot be predeclared.
    
    Depends On: D146201
    
    Reviewed By: dcaballe
    
    Differential Revision: https://reviews.llvm.org/D146210

--- mlir/lib/Dialect/Affine/Utils/LoopUtils.cpp
+++ mlir/lib/Dialect/Affine/Utils/LoopUtils.cpp
@@ -2368,8 +2368,8 @@ static bool getFullMemRefAsRegion(Operation *op, unsigned numParamLoopIVs,
   for (unsigned d = 0; d < rank; d++) {
     auto dimSize = memRefType.getDimSize(d);
     assert(dimSize > 0 && "filtered dynamic shapes above");
-    regionCst->addBound(IntegerPolyhedron::LB, d, 0);
-    regionCst->addBound(IntegerPolyhedron::UB, d, dimSize - 1);
+    regionCst->addBound(BoundType::LB, d, 0);
+    regionCst->addBound(BoundType::UB, d, dimSize - 1);
   }
   return true;
 }
