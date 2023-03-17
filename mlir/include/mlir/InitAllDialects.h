445652f08cb5ae00b51d488ffd3ad4c1d06cd8fd
Matthias Springer <me@m-sp.org>
2023-03-17 02:33:26 +0000

    [mlir][linalg] Add ValueBoundsOpInterface impls for Linalg structured ops
    Also add a few more complex test cases.
    
    Depends On: D145804
    
    Differential Revision: https://reviews.llvm.org/D145806

--- mlir/include/mlir/InitAllDialects.h
+++ mlir/include/mlir/InitAllDialects.h
@@ -40,6 +40,7 @@
 #include "mlir/Dialect/LLVMIR/NVVMDialect.h"
 #include "mlir/Dialect/LLVMIR/ROCDLDialect.h"
 #include "mlir/Dialect/Linalg/IR/Linalg.h"
+#include "mlir/Dialect/Linalg/IR/ValueBoundsOpInterfaceImpl.h"
 #include "mlir/Dialect/Linalg/TransformOps/LinalgTransformOps.h"
 #include "mlir/Dialect/Linalg/Transforms/BufferizableOpInterfaceImpl.h"
 #include "mlir/Dialect/Linalg/Transforms/TilingInterfaceImpl.h"
@@ -139,6 +140,7 @@ inline void registerAllDialects(DialectRegistry &registry) {
       registry);
   linalg::registerBufferizableOpInterfaceExternalModels(registry);
   linalg::registerTilingInterfaceExternalModels(registry);
+  linalg::registerValueBoundsOpInterfaceExternalModels(registry);
   memref::registerBufferizableOpInterfaceExternalModels(registry);
   memref::registerRuntimeVerifiableOpInterfaceExternalModels(registry);
   memref::registerValueBoundsOpInterfaceExternalModels(registry);
