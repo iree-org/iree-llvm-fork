226510cb086ba1066bbcad14fea13eb1bc21af03
Hanhan Wang <hanchung@google.com>
2023-08-10 13:53:55 -0700

    fix fold-memref-alias-op crash

--- mlir/lib/Dialect/Linalg/Transforms/Vectorization.cpp
+++ mlir/lib/Dialect/Linalg/Transforms/Vectorization.cpp
@@ -1489,7 +1489,8 @@ static LogicalResult reductionPreconditions(LinalgOp op) {
 static LogicalResult vectorizeDynamicLinalgOpPrecondition(linalg::LinalgOp op) {
   // TODO: Masking only supports dynamic generic ops for now.
   if (!isa<linalg::GenericOp, linalg::FillOp, linalg::CopyOp,
-           linalg::ContractionOpInterface>(op.getOperation()))
+           linalg::TransposeOp, linalg::ContractionOpInterface>(
+          op.getOperation()))
     return failure();
 
   LDBG("Dynamically-shaped op meets vectorization pre-conditions\n");
